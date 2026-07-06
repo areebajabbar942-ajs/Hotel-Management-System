adbb2a8094e61264f8ca95bdae184ede7a14612d
areebajabbar942-ajs <areebajabbar942@gmail.com>
2026-07-06 23:44:30 +0500

    Update rooms.cpp

--- rooms.cpp
+++ rooms.cpp
@@ -480,16 +480,4 @@ bool checkAvailability(int roomNo)
 	return false;
 }
 
-int getDaysInMonth(int month)
-{
-	if (month == 2) {
-		return 28;
-	}
-	else if (month == 4 || month == 6 || month == 9 || month == 11) {
-		return 30;
-	}
-	else {
-		return 31;
-	}
 
-}
